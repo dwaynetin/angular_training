93e1239c043e2545eecc7707344924a5d84a4e8a
Dwayne Sanson Tin <dwaynetin@gmail.com>
2019-12-02 02:32:25 +0800

    Split the Routing

--- ngASATraining/src/app/app-routing.module.ts
+++ ngASATraining/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BranchesComponent } from './branches/branches.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'branches', component: BranchesComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
